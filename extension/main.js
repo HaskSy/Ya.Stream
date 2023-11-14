caca6a94325ddb35035ab489ba32f6c169e0fb3e
Sergo Kovaltsov <sergeykovaltsov@gmail.com>
2023-11-14 11:27:57 +0300

    fixed errors in extention

--- extension/main.js
+++ extension/main.js
@@ -1,3 +1,11 @@
-if (!localStorage.hasOwnProperty('history')) {
-    localStorage.setItem('history', "[]")
+function init() {
+    const chromeLocalStorage = chrome.storage.local
+    
+    chromeLocalStorage.get(["history"], (history) => {
+        if (history === undefined || !history) {
+            chromeLocalStorage.set({"history": []})
+        }
+    })
 }
+
+init()
