6245932031c51cb1900dbcdae94925b4c0804d18
dae1223 <dae1223@yandex.ru>
2023-11-15 19:21:53 +0300

    ui

--- extension/popup.js
+++ extension/popup.js
@@ -34,33 +34,189 @@ if (token != null) {
 
 
 /** -------------------- functions.js -------------------- */
-const connectBtn = document.getElementById("connectBtnId");
+const chromeLocalStorage = chrome.storage.local;
+const stream_switcher = document.getElementById("switcher");
+const status_description = document.getElementById("status");
+const text_form = document.getElementById("textForm");
+const listen_button = document.getElementById("connectBtnId");
+const auth_button = document.getElementById("authBtn");
+const normal_display = document.getElementById("if_auth");
+const not_auth_display = document.getElementById("if_not_auth");
 const btnArr = ['l1', 'l2', 'l3', 'l4', 'l5']
-connectBtn.addEventListener("click",async ()=> {
-    const nextUser = document.getElementById("textForm").value;
-    if (nextUser != ""){
-            nextUserIndex = historyArr.indexOf(nextUser);
-        if (nextUserIndex != -1){
-            historyArr.splice(nextUserIndex, 1);
-        }
-        else{
-            historyArr.pop();
-        }
-        
-        historyArr = [nextUser].concat(historyArr);
-        chrome.storage.local.set({history: historyArr}).then(() => "Setting succesfully");  
-        refreshBtns()
+
+class State{
+	static setIsStreaming(flag){
+		chromeLocalStorage.set({isStreaming: flag}).then(() => "Setting succesfully");
+	}
+
+	static async getIsStreaming(){
+		return (await chrome.storage.local.get(['isStreaming']))['isStreaming'];
+	}
+	
+	static setIsListening(flag){
+		chromeLocalStorage.set({isListening: flag}).then(() => "Setting succesfully");
+	}
+
+	static async getIsListening(){
+		return (await chrome.storage.local.get(['isListening']))['isListening'];
+	}
+
+	static setAuthenticated(token) {
+		let obj = {}
+		obj[config.authTokenStorageLocation] = token
+		chrome.storage.local.set(obj);
+	}
+
+	static async getAuthenticated() {
+		const result = await chrome.storage.local.get(config.authTokenStorageLocation);
+		return result[config.authTokenStorageLocation] || null
+	}
+
+	static async isAuthenticated() {
+		return (null !== await this.getAuthenticated())
+	}
 }
-})
+
+function ui_notifyStartListening(user_id)
+{
+	switcher.checked = false;
+	status_description.innerText = "Now listening " + user_id;
+	rearrangeBtns(user_id);
+	listen_button.textContent = "Stop listening";
+	listen_button.style.background = "linear-gradient(#e10101, #e10101)";
+}
+
+function ui_notifyStopListening()
+{
+	status_description.innerHTML = "Listening is stoped";
+	listen_button.textContent = "Start listening";
+	listen_button.style.background = "linear-gradient(#01a9e1, #5bc4bc)";
+}
+
+function ui_notifyStartStreaming()
+{
+	switcher.checked = true;
+	status_description.innerHTML = "Now streaming";
+}
+
+function ui_notifyStopStreaming()
+{
+	switcher.checked = false;
+	status_description.innerText = "Streaming is stoped";
+}
+
+function startListening(user_id){
+	stopStreaming();
+	stopListening();
+	State.setIsListening(user_id);
+	//ListenerService.startListening(user_id);
+	ui_notifyStartListening(user_id);
+}
+
+function stopListening(){
+	State.setIsListening(null);
+	//ListenerService.stopListening();
+	ui_notifyStopListening();
+}
+
+function startStreaming(){
+	stopListening();
+	//streamingService.start();
+	ui_notifyStopListening();
+	ui_notifyStartStreaming();
+	State.setIsStreaming(true);
+}
+
+function stopStreaming(){
+	//streamingService.stop();
+	State.setIsStreaming(false);
+	ui_notifyStopStreaming();
+}
+
 
 window.onload = async () => {
+	document.getElementById("if_not_auth").style.display = "none"
+	refreshBtns();
+    if ((await State.getIsStreaming()) == true){
+		ui_notifyStartStreaming();
+	}
+	else if ((await State.getIsListening()) != null){
+		ui_notifyStartListening(await State.getIsListening());
+	}
+}
+
+listener();
+
+function listener(){
+	switcher = document.getElementById("switcher");
+	switcher.addEventListener('click', async function() {
+		if (this.checked) {
+			//State.setAuthenticated(null);
+			if (!await State.isAuthenticated()){
+				normal_display.style.display = "none"
+				not_auth_display.style.display = "block"
+			}
+			else{
+				startStreaming();
+			}
+		} else {
+			stopStreaming();
+		}
+	  });
+	
+    
+    listen_button.addEventListener("click",async ()=> {
+		if ((await State.getIsListening()) != null){
+			stopListening();
+		}
+		else{
+			//State.setAuthenticated("token")
+			if (!await State.isAuthenticated()){
+				normal_display.style.display = "none"
+				not_auth_display.style.display = "block"
+			}
+			else{
+				nextUser = text_form.value;
+				if (nextUser != ""){
+					startListening(nextUser);
+				}
+			}
+		}
+    })
+
+    btnArr.forEach(element => {
+        last5btn = document.getElementById(element);
+        last5btn.addEventListener("click",async ()=> {
+            text_form.value = document.getElementById(element).textContent;
+        })
+    });
+
+	auth_button.addEventListener("click",async ()=> {
+		//login();
+		normal_display.style.display = "block"
+		not_auth_display.style.display = "none"
+    })
+}
+
+async function rearrangeBtns(nextUser){
+    historyArr = (await chrome.storage.local.get(['history']))['history'];
+    nextUserIndex = historyArr.indexOf(nextUser);
+    if (nextUserIndex != -1){
+        historyArr.splice(nextUserIndex, 1);
+    }
+    else{
+        historyArr.pop();
+    }
+    
+    historyArr = [nextUser].concat(historyArr);
+    chromeLocalStorage.set({history: historyArr}).then(() => "Setting succesfully");  
     refreshBtns()
 }
 
 async function refreshBtns() {
-    const historyArr = (await chrome.storage.local.get(['history']))['history']
+    historyArr = (await chromeLocalStorage.get(['history']))['history']
     for (let index = 0; index < btnArr.length; index++) {
-        const lastBtn = document.getElementById(btnArr[index]);
+        lastBtn = document.getElementById(btnArr[index]);
         lastBtn.textContent = historyArr[index];
     }
 }
@@ -168,19 +324,17 @@ function setAuthToken(token) {
  */
 class ListenerService {
 
-	constructor() {
 		/** @type {string} */
-		this.currentUser = "";
+		static currentUser = "";
 		/** @type {EventSource | undefined} */
-	  	this.listenedSource = undefined;
-	}
+	  	static listenedSource = undefined;
 
 	/**
 	 * Event handler for when the connection is open.
 	 * @param {Event} e - Generic open event.
 	 * @returns {void}
 	 */
-  	#onOpenHandler = (e) => {
+  	static #onOpenHandler = (e) => {
 		console.log(`The connection with user "${this.currentUser}" has been (re)establised`);
   	}
 	/**
@@ -188,22 +342,22 @@ class ListenerService {
      * @param {MessageEvent<ActionEvent>} event - Message event.
      * @returns {void}
      */
-  	#onMessageHandler = (event) => console.log(event.data);
+  	static #onMessageHandler = (event) => console.log(event.data);
 
 	/**
      * Event handler for errors.
      * @param {Event} error - Generic error event.
      * @returns {void}
      */
-	#onErrorHandler = (error) => console.error(error);
+	static #onErrorHandler = (error) => console.error(error);
 
-	#setListeners() {
+	static #setListeners() {
 		this.listenedSource.addEventListener("open", this.#onOpenHandler)
 		this.listenedSource.addEventListener("message", this.#onMessageHandler)
 		this.listenedSource.addEventListener("error", this.#onErrorHandler)
 	}
 
-	#unsetListeners() {
+	static #unsetListeners() {
 		this.listenedSource.removeEventListener("open", this.#onOpenHandler)
 		this.listenedSource.removeEventListener("message", this.#onMessageHandler)
 		this.listenedSource.removeEventListener("error", this.#onErrorHandler)
@@ -214,7 +368,7 @@ class ListenerService {
 	 * @param {string} userId - Yandex ID of the user to listen to.
 	 * @returns {void}
 	 */
-	async startListening(userId) {
+	static async startListening(userId) {
 	  	this.stopListening();
 		this.listenedSource = createEventSourceListener(userId)
 		this.currentUser = userId;
@@ -225,7 +379,7 @@ class ListenerService {
    	 * Stop listening to real-time events.
    	 * @returns {void}
    	 */
-	stopListening() {
+	static stopListening() {
 	  	if (this.listenedSource) {
 			this.#unsetListeners();
 			this.listenedSource.close();
