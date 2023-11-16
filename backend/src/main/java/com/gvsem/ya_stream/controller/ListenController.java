718d5842d31a6b7f1654b4f611e0da1bb63a8bfc
Georgii Semenov <themrgeorgeous@gmail.com>
2023-11-16 11:48:31 +0300

    changed to postgres, added /listen/demo

--- backend/src/main/java/com/gvsem/ya_stream/controller/ListenController.java
+++ backend/src/main/java/com/gvsem/ya_stream/controller/ListenController.java
@@ -1,5 +1,6 @@
 package com.gvsem.ya_stream.controller;
 
+import com.gvsem.ya_stream.model.event.Event;
 import com.gvsem.ya_stream.model.user.User;
 import com.gvsem.ya_stream.model.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,7 +30,28 @@ public class ListenController {
 
     @GetMapping(path = "/listen/{yandexLogin}", produces= MediaType.TEXT_EVENT_STREAM_VALUE)
     ResponseEntity<SseEmitter> listen(@PathVariable("yandexLogin") String yandexLogin) {
-        User user = userService.getUserByYandexLogin(yandexLogin).orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "User not found"));
+        if (yandexLogin.equals("demo")) {
+            User demoUser = new User();
+            demoUser.setId(1L);
+            demoUser.setYandexLogin("demo");
+            (new Thread(() -> {
+                try {
+                    Thread.sleep(1000);
+                    sseBus.send(Event.play(demoUser, "23157104:106317038", "120"), demoUser);
+                    Thread.sleep(11000);
+                    sseBus.send(Event.play(demoUser, "5551948:42125196", "169"), demoUser);
+                    Thread.sleep(11000);
+                    sseBus.send(Event.play(demoUser, "3369429:28192686", "150"), demoUser);
+                    Thread.sleep(25000);
+                    sseBus.send(Event.stop(demoUser, "3369429:28192686", "0"), demoUser);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            })).start();
+        } else {
+            userService.getUserByYandexLogin(yandexLogin).orElseThrow(() -> new ResponseStatusException(NOT_FOUND, "User not found"));
+        }
+
         var emitter = new SseEmitter(-1L);
         sseBus.subscribe(emitter, yandexLogin);
         return ResponseEntity.status(HttpStatus.OK).body(emitter);
